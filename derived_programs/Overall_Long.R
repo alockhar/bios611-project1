a8eda8cda4e6f0572b80c2f22a15766e46d61a0e
Alexandre Lockhart <Alexandre_georges@hotmail.com>
2020-09-14 20:28:13 -0400

    Derived programs

--- derived_programs/Overall_Long.R
+++ derived_programs/Overall_Long.R
@@ -6,12 +6,12 @@ library(dplyr)
 IP=read_csv("derived_data/Overall.csv");
 
 
-Sub1<-IP%>%mutate(deaths=DeathsSideA)%>%select(WarNum,deaths,WDuratDays,Americas,WarTypeD,Initiator,SideA)
+Sub1<-IP%>%mutate(deaths=DeathsSideA)%>%select(WarNum,deaths,WDuratDays,Outcome,Americas,WarTypeD,Initiator,SideA)
 Sub1$side='A'
 Sub1$Init=ifelse(Sub1$SideA==Sub1$Initiator,1,0)
 SubA<-Sub1%>%select(-c(Initiator,SideA))
 
-Sub2<-IP%>%mutate(deaths=DeathsSideB)%>%select(WarNum,deaths,WDuratDays,Americas,WarTypeD,Initiator,SideB)
+Sub2<-IP%>%mutate(deaths=DeathsSideB)%>%select(WarNum,deaths,WDuratDays,Outcome,Americas,WarTypeD,Initiator,SideB)
 Sub2$side='B'
 Sub2$Init=ifelse(Sub2$SideB==Sub2$Initiator,1,0)
 SubB<-Sub2%>%select(-c(Initiator,SideB))
