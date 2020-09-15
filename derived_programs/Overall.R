b8938bee87c5fc8bc8085c56ad13c4076202e78a
Alexandre Lockhart <Alexandre_georges@hotmail.com>
2020-09-14 20:11:13 -0400

    derived programs

---
+++ derived_programs/Overall.R
@@ -0,0 +1,44 @@
+library(dplyr)
+
+
+#R script to generate additional derived variables from Overall dataset
+
+##################
+#Present overall descriptive statistics from central nations in conflict
+##################
+
+
+Overall=read_csv("source_data/INTRA-STATE WARS v5.1 CSV.csv");
+
+IP<-Overall%>% mutate(WarTypeC = as.factor(case_when(
+  WarType==4~'Civil War: Central Control',WarType==5~'Civil War: Local Issues',WarType==6~'Regional Internal',WarType==7~'Intercommunal',WarType==-8~'NA' )),
+  V5RegionC=case_when(V5RegionNum==1~'North America',V5RegionNum==2~'South America',V5RegionNum==3~'Europe',V5RegionNum==4~'Sub-Saharan Africa',V5RegionNum==5~'Middle East and North Africa',V5RegionNum==6~'Asia and Oceania'),OutcomeC=case_when(Outcome==1~"Side A wins",Outcome==2~"Side B wins",Outcome==3~"Compromise",Outcome==4~"War transformed into another War",Outcome==5~"War ongoing as of end of 2014",Outcome==6~"Stalemate",Outcome==7~"Conflict continues below war level" ) )
+
+IP$WarTypeD=ifelse(IP$WarType==4,'Civil War: Central Control',ifelse(IP$WarType==5,'Civil War: Local Issues',ifelse(IP$WarType%in%c(6,7),'Other','NA')))
+
+
+#Create a variable for set upper limit to deaths 50000 after checking limit to deaths
+IP$TotalBDeathsU=ifelse(IP$TotalBDeaths>=50000,50000,IP$TotalBDeaths)
+
+#Make Hemisphere driven variable
+IP$Americas=ifelse(IP$V5RegionNum %in% c(1,2),'Americas','Not in Americas')
+
+#Let's make some dates
+
+IP$Phase1_st=ymd( paste(IP$StartYr1, IP$StartMo1, IP$StartDy1, sep="-"))
+IP$Phase2_st=ymd( paste(IP$StartYr2, IP$StartMo2, IP$StartDy2, sep="-"))
+IP$Phase3_st=ymd( paste(IP$StartYr3, IP$StartMo3, IP$StartDy3, sep="-"))
+IP$Phase4_st=ymd( paste(IP$StartYr4, IP$StartMo4, IP$StartDy4, sep="-"))
+
+IP$Phase1_en=ymd( paste(IP$EndYr1, IP$EndMo1, IP$EndDy1, sep="-"))
+IP$Phase2_en=ymd( paste(IP$EndYr2, IP$EndMo2, IP$EndDy2, sep="-"))
+IP$Phase3_en=ymd( paste(IP$EndYr3, IP$EndMo3, IP$EndDy3, sep="-"))
+IP$Phase4_en=ymd( paste(IP$EndYr4, IP$EndMo4, IP$EndDy4, sep="-"))
+
+
+#Consider making transition to and from variables
+
+
+#End Derived program for overall data set
+write_csv(IP,"derived_data/Overall.csv")
+
---
+++ derived_programs/Overall_Long.R
@@ -0,0 +1,26 @@
+library(dplyr)
+
+
+#R script to make Overall dataset into long format
+
+IP=read_csv("derived_data/Overall.csv");
+
+
+Sub1<-IP%>%mutate(deaths=DeathsSideA)%>%select(WarNum,deaths,WDuratDays,Americas,WarTypeD,Initiator,SideA)
+Sub1$side='A'
+Sub1$Init=ifelse(Sub1$SideA==Sub1$Initiator,1,0)
+SubA<-Sub1%>%select(-c(Initiator,SideA))
+
+Sub2<-IP%>%mutate(deaths=DeathsSideB)%>%select(WarNum,deaths,WDuratDays,Americas,WarTypeD,Initiator,SideB)
+Sub2$side='B'
+Sub2$Init=ifelse(Sub2$SideB==Sub2$Initiator,1,0)
+SubB<-Sub2%>%select(-c(Initiator,SideB))
+
+
+Long=data.frame(rbind(SubA,SubB))
+
+
+
+#End Derived program for overall data set
+write_csv(Long,"derived_data/Overall_Long.csv")
+
